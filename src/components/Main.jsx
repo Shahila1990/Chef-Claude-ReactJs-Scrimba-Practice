ec21c23760f9cfe99580aea85ca7def09279354d
Shahila Mafraz <fathima.shahila371@gmail.com>
2025-04-23 10:47:27 +0800

    created new component to cleanup main code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,24 @@
 import React from "react"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
 
 export default function Main() {
-    const [ingredients , setIngredients] = React.useState([])
-    const ingredientsListItems = ingredients.map(ingredient => (<li key={ingredient}>{ingredient}</li>))
+    const [ingredients, setIngredients] = React.useState(
+        ["all the main spices", "pasta", "ground beef", "tomato paste"]
+    )
+    
 
+    const [recipeShown, setRecipeShown] = React.useState(false)
+
+    function showRecipe(){
+        setRecipeShown(prevShown => !prevShown)
+
+    }
 
     function addIngredients(formData) {
-        
+
         const newIngredient = formData.get('ingredient')
-        setIngredients(prevIngredient => [...prevIngredient , newIngredient])
+        setIngredients(prevIngredient => [...prevIngredient, newIngredient])
 
 
 
@@ -28,19 +38,13 @@ export default function Main() {
                         Add Ingredients
                     </button>
                 </form>
-                {ingredients.length > 0 && <section>
-                    <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list" aria-live="polite">
-                        {ingredientsListItems}
-                    </ul>
-                    {ingredients.length > 3 && <div className="get-recipe-container">
-                        <div>
-                            <h3>Ready for a recipe?</h3>
-                            <p>Generate a recipe from your list of ingredients.</p>
-                        </div>
-                        <button>Get a recipe</button>
-                    </div>}
-                </section>}
+                {ingredients.length > 0 && <IngredientsList 
+                    showRecipe={showRecipe}
+                    ingredients={ingredients}
+
+                />}
+
+                {recipeShown && <ClaudeRecipe />}
 
             </main>
 
